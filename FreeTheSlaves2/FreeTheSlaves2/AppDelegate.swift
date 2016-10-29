24f4fad0b40e21796229af6e96dcaf4fb923f67f
Janson Lau <jansonla@usc.edu>
2016-10-29 01:18:26 -0400

    import firebase/d firebase/database

--- FreeTheSlaves2/FreeTheSlaves2/AppDelegate.swift
+++ FreeTheSlaves2/FreeTheSlaves2/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FIRDatabase.database().persistenceEnabled = true
         FIRApp.configure()
         return true
     }
