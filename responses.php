9c9ce2ff796a40dbbf04cb0ff4e7dc98452da0ea
Yuming Qin <yumingq@wharton.upenn.edu>
2016-10-29 00:38:37 -0400

    Adding a response php file

---
+++ responses.php
@@ -0,0 +1,8 @@
+<?php
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+?>
+<Response>
+    <!-- <Message>WOW WOW WOW thanks, Mobile Monkey</Message> -->
+    <SMS>SMS WOW WOW WOW thanks, Mobile Monkey SMS</SMS>
+</Response>
