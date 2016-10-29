3e12d4e702d452dfc8424ee4e16a3309fcda7ee1
Force <force@ip-172-31-11-220.ec2.internal>
2016-10-29 08:01:51 +0000

    languagebranch.php commit

---
+++ languagebranch.php
@@ -0,0 +1,15 @@
+<?php
+
+	 if($_REQUEST['Digits'] === '1') {
+        header("Location: englishq1.php);
+    }
+
+	if($_REQUEST['Digits'] === '2') {
+        header("Location: hindiq1.php);
+    }
+
+	if($_REQUEST['Digits'] === '3') {
+        header("Location: frenchq1.php);
+    }
+
+?>
