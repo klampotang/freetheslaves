469246455ed6c9c7315902e84079e585cdc7d916
Janson Lau <jansonla@usc.edu>
2016-10-28 22:28:42 -0400

    enter buttons

--- FreeTheSlaves2/FreeTheSlaves2/AViewController.swift
+++ FreeTheSlaves2/FreeTheSlaves2/AViewController.swift
@@ -21,6 +21,9 @@ class AViewController: UIViewController {
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
     }
+    @IBAction func enterPressed(_ sender: Any) {
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
