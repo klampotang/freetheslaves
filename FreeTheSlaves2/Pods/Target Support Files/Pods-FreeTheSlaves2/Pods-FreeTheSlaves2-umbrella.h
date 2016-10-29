093c09947c3f9d92daebd7b5f432d8ea8289ee66
Janson Lau <jansonla@usc.edu>
2016-10-29 00:51:58 -0400

    jesus christ my podfile finally freaking worked

---
+++ FreeTheSlaves2/Pods/Target Support Files/Pods-FreeTheSlaves2/Pods-FreeTheSlaves2-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_FreeTheSlaves2VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FreeTheSlaves2VersionString[];
+
---
+++ FreeTheSlaves2/Pods/Target Support Files/Pods-FreeTheSlaves2Tests/Pods-FreeTheSlaves2Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FreeTheSlaves2Tests : NSObject
+@end
+@implementation PodsDummy_Pods_FreeTheSlaves2Tests
+@end
