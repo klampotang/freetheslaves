f99c4772b9db65705caf540efcfd1d2cd1495413
Force <force@ip-172-31-11-220.ec2.internal>
2016-10-29 08:38:12 +0000

    main.php addition, php is the worst #EpicFail

---
+++ main.php
@@ -0,0 +1,25 @@
+<?php
+	header("Location: languagemenu.php");
+	header("Location: languagebranch.php");
+	header("Location: englishq1.php");
+	header("Location: englisha1.php");
+	header("Location: hindiq1.php");
+	header("Location: hindia1.php");
+	header("Location: frenchq1.php");
+	header("Location: frencha1.php");
+	header("Location: englishq2.php");
+	header("Location: englisha2.php");
+	header("Location: hindiq2.php");
+	header("Location: hindia2.php");
+	header("Location: frenchq2.php");
+	header("Location: frencha2.php");
+	header("Location: englishq3.php");
+	header("Location: englisha3.php");
+	header("Location: hindiq3.php");
+	header("Location: hindia3.php");
+	header("Location: frenchq3.php");
+	header("Location: frencha3.php");
+
+?>
+
+
